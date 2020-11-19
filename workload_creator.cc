62ad0669e79800a273ce3af94ae8bb404017c1bc
shinhojin <ghwls03s@gmail.com>
2020-11-19 10:11:55 +0900

    add seq/rand/zipfian workload create function

--- workload_creator.cc
+++ workload_creator.cc
@@ -1,88 +1,124 @@
-#include "m2controller.h"
 #include "workload_creator.h"
-#include <fstream>
+
 #include <string.h>
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
 
-Workload_Data * Workload_Creator::get_workload_data(string data) {
-    char * token_buffer = new char[200];
-    int token_SN;
-    double token_time;
-    int token_PID;
-    string token_event;
-    int token_start_lba;
-    int token_blocks;
-    string token_process;
+Workload_Creator::Workload_Creator(zns_share_info * zonelist, int zone_count, int update_count) {
+    this->zns_info_list = zonelist;
 
-    static int count = 0;
-
-    strcpy(token_buffer, data.c_str());
-    
-    char * token = strtok(token_buffer, " ");
-    token_SN = atoi(token);
-
-    token = strtok(NULL, " ");
-    token_time = atof(token);
-
-    token = strtok(NULL, " ");
-    token_PID = atoi(token);
-
-    //pass
-    token = strtok(NULL, " ");
-    
-    token = strtok(NULL, " ");
-    token_event = string(token);
-
-    token = strtok(NULL, " ");
-    token_start_lba = atoi(token);
-
-    token = strtok(NULL, " ");
-    token_blocks = atoi(token);
-
-    //pocess temp
-    token_process = string("NA");
-
-    delete token_buffer;
-
-    count++;
-    return new Workload_Data(token_SN, token_time, token_PID, token_event, token_start_lba, token_blocks, token_process);
+    this->Zone_count = zone_count;
+    this->Update_count = update_count;
 }
 
-void Workload_Creator::get_workload_job(string path) {
-    ifstream readFile;
-    string raw_data;
-    Workload_Data * data;
+int *Workload_Creator::create_sequential_workload(SIM_Zone * Zone_bitmap, SIM_Segment * Segment_bitmap, SIM_Block * Block_bitmap) {
+    update_block = new int[Update_count];
+    int seq_num;
+    int total_block_num = Zone_count * 512 * 512 - 1;
 
-    int count = 0;
-
-    readFile.open(path);
-
-    if(!readFile.is_open()) {
-        cout<< "fail open workload data file" <<endl;
+    if (Update_count > total_block_num) {
+        cout << "Too many update count!" << endl;
+        cout << "Please set update count under " << total_block_num << endl;
+        exit(0);
     }
+
+
+
     
-    while(getline(readFile, raw_data)) {
-        data = get_workload_data(raw_data);
-        //data->print_workload_data();
-        workload_list->push_back(data);
-        count++;
-    }
-
-    cout<<"Data Line : "<< count <<endl;
-    readFile.close();
 }
 
-void Workload_Creator::print_all_workload() {
-    if(workload_list->size() == 0) {
-        cout<< "Workload empty" <<endl;
-        return;
+int *Workload_Creator::create_random_workload(SIM_Zone * Zone_bitmap, SIM_Segment * Segment_bitmap, SIM_Block * Block_bitmap) {
+    update_block = new int[Update_count];
+    srand(time(NULL));
+
+    int total_block_num = Zone_count * 512 * 512 - 1;
+
+    if (Update_count > total_block_num) {
+        cout << "Too many update count!" << endl;
+        cout << "Please set update count under " << total_block_num << endl;
+        exit(0);
     }
 
-    list<Workload_Data * >::iterator iter;
-    for (iter = workload_list->begin(); iter != workload_list->end(); ++iter){
-        (*iter)->print_workload_data();
+    for(int i  = 0; i < Update_count; i++) {
+        update_block[i] = rand() % total_block_num;
+    }
+
+    // Sorting Block number
+    ascending_sort(update_block);
+
+    return update_block;
+}
+
+int *Workload_Creator::create_zipfian_workload(SIM_Zone * Zone_bitmap, SIM_Segment * Segment_bitmap, SIM_Block * Block_bitmap) {
+    update_block = new int[Update_count];
+}
+
+void Workload_Creator::ascending_sort(int * block_sort) {
+    int temp;
+
+    for (int i = 0; i < Update_count; i++) {
+        for (int j = i; j < Update_count; j++) {
+            if (block_sort[i] > block_sort[j]) {
+                temp = block_sort[i];
+                block_sort[i] = block_sort[j];
+                block_sort[j] = temp;
+            }
+        }
     }
 }
 
-list<Workload_Data * > * Workload_Creator::getWorkloadlist() {
-    return workload_list;
+int Workload_Creator::update_block_in_memory(SIM_Zone * Zone_bitmap, SIM_Segment * Segment_bitmap, SIM_Block * Block_bitmap, int * _update_bitmap) {
+    int start_block = 0;
+    int end_block = Zone_count * 512 * 512;
+    int update_cnt = 0;
+    int io_result = 0;
+    int sel_zone = Zone_count;
+    int write_offset = 0;
+    int i_current_write_buffer = 0;
+    
+    void * dummy_data = new char[ZNS_BLOCK_SIZE * 32];
+    memset(dummy_data, 66, ZNS_BLOCK_SIZE * 32);
+
+    cout << "Print Zone Utilization Before Update" << endl;
+    cout<< "------------------------------------------------------" <<endl;
+    for (int i = 0; i < Zone_count; i++) {
+        cout << "Zone " << i << " : " << Zone_bitmap[i].get_utilization() << endl; 
+    }
+    cout<< "------------------------------------------------------" <<endl;
+
+    cout << "Update in memory ...ing" << endl;
+    for (int i_block = start_block; i_block < end_block; i_block++) {
+        for (int j_block = 0; j_block < Update_count; j_block++) {
+            if (i_block == _update_bitmap[j_block]) {
+                Block_bitmap[i_block].set_state(INVALID_BLOCK);
+                update_cnt++;
+                //cout << "through here 1" << endl;
+                break;
+            }
+        }
+        if (update_cnt == 32) {
+            //cout << "through here 2" << endl;
+            io_result = m2_zns_write(zns_info_list, dummy_data, (512 * 8 * 32), sel_zone, write_offset * 8);
+            if (io_result == 0) {
+                write_offset += update_cnt;
+                update_cnt = 0;
+            } else {
+                cout << "ZNS SSD Write Fail" << endl;
+                exit(0);
+            }
+        }
+    }
+    if (update_cnt < 32) {
+        //io_result = m2_zns_write(zns_info_list, dummy_data, (512 * 8 * 32), sel_zone, write_offset * 8);
+        if (io_result == 0) {
+            write_offset += update_cnt;
+            update_cnt = 0;
+        } else {
+            cout << "ZNS SSD Write Fail" << endl;
+            exit(0);
+        }
+    }
+    cout << "Finish Update in memory" << endl;
+    return write_offset;
 }
--- workload_creator.h
+++ workload_creator.h
@@ -1,24 +1,37 @@
-#ifndef Workload_creator_H
-#define Workload_creator_H
+#ifndef workload_creator_H
+#define workload_creator_H
 
-#include <list>
-#include "workload_data.h"
+#include "zns_simulation_datastructure.h"
+#include "zns_simulation.h"
+#include <iostream>
 
-#define WORKLOAD_BUFFER_SIZE 1024
+//Control zone, segment, block state
+#define VALID_BLOCK 1
+#define INVALID_BLOCK 2
+#define FREE_BLOCK 0
 
 using namespace std;
 
 class Workload_Creator {
-    list<Workload_Data * > * workload_list;
+    //ZNS SSD variable
+    struct zns_share_info * zns_info_list;
+
+    int * update_block;
+    int Zone_count;
+    int Update_count;
+
 public :
-    ~Workload_Creator() {};
-    Workload_Creator() {
-        workload_list = new list<Workload_Data * >;
-    };
-    Workload_Data * get_workload_data(string data);
-    void get_workload_job(string path);
-    void print_all_workload();
-    list<Workload_Data * > * getWorkloadlist();
+    Workload_Creator(zns_share_info * zonelist, int zone_count, int update_count);
+
+    // create workload function
+    int *create_sequential_workload(SIM_Zone * Zone_bitmap, SIM_Segment * Segment_bitmap, SIM_Block * Block_bitmap);
+    int *create_random_workload(SIM_Zone * Zone_bitmap, SIM_Segment * Segment_bitmap, SIM_Block * Block_bitmap);
+    int *create_zipfian_workload(SIM_Zone * Zone_bitmap, SIM_Segment * Segment_bitmap, SIM_Block * Block_bitmap);
+
+    void ascending_sort(int * block_sort);
+
+    // update block function
+    int update_block_in_memory(SIM_Zone * Zone_bitmap, SIM_Segment * Segment_bitmap, SIM_Block * Block_bitmap, int * _update_bitmap);
 };
 
 #endif
