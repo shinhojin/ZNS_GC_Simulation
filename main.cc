66e2c5aa68cbd66d2cb43b5524080b32b7b6b3cb
shinhojin <ghwls03s@gmail.com>
2020-11-27 16:06:42 +0900

    add u3 zns ssd test code

--- main.cc
+++ main.cc
@@ -15,6 +15,16 @@ using namespace std;
 int main(int argc, char * argv[]) {
     ZNS_Simulation * zns_simulation;
 
+    void * test_data = malloc(_192KB * 1);
+	memset(test_data, 'W', _192KB * 1);
+
+    //ZNS SSD Init
+	zns_get_info("/dev/nvme0n1");
+    cout << "do" << endl;
+	zns_get_zone_desc(REPORT_ALL, REPORT_ALL_STATE, 0, 0, true);
+
+    u3_zns_write(test_data, _192KB * 1, 0);
+
     if(argc != ARGUMENT_COUNT) {
         cout<< "Please, set argument" <<endl;
         cout<< "---> sudo ./Simulation {DEV_NAME} {ZONE_COUNT(MAX_512)} {SETTING_ZONE_UTILIZATION(%)} {GC_NUMBER(1.BASIC_GC, 2.LSM_ZGC)} {WORKLOAD_TYPE(1.SEQ, 2.RAND, 3.ZIPFIAN)} {UPDATE_COUNT}" <<endl;
@@ -33,7 +43,8 @@ int main(int argc, char * argv[]) {
     //1. Init ZNS SSD
     cout<< "1. Start Init ZNS SSD Simulation" <<endl;
     cout<< "------------------------------------------------------" <<endl;
-        zns_simulation = new ZNS_Simulation(argv[DEV_NAME], atoi(argv[ZONE_COUNT]), atoi(argv[SETTING_DEV_UTILIZATION]), atoi(argv[UPDATE_COUNT]));
+        //zns_simulation = new ZNS_Simulation(argv[DEV_NAME], atoi(argv[ZONE_COUNT]), atoi(argv[SETTING_DEV_UTILIZATION]), atoi(argv[UPDATE_COUNT]));
+        //zns_simulation->print_segment_block_bitmap(0);
         
     cout<< "------------------------------------------------------" <<endl<<endl;
     
@@ -45,7 +56,7 @@ int main(int argc, char * argv[]) {
             //zns_simulation->request_sequential_workload();
         } else if (atoi(argv[WORKLOAD_NUM]) == 2) {
             cout << "Do Random" << endl;
-            zns_simulation->request_random_workload();
+            //zns_simulation->request_random_workload();
         } else if (atoi(argv[WORKLOAD_NUM]) == 3) {
             cout << "Do Zipfian" << endl;
             //zns_simulation->request_zipfian_workload();
@@ -58,7 +69,8 @@ int main(int argc, char * argv[]) {
     cout<< "3. Start Workload Simualtion in memory" <<endl;
     cout<< "------------------------------------------------------" <<endl;
         cout << "Do Update" << endl;
-        zns_simulation->request_update_workload();
+        //zns_simulation->request_update_workload();
+        //zns_simulation->print_segment_block_bitmap(0);
     cout<< "------------------------------------------------------" <<endl<<endl;
 
     //4. Start GC Simulation in ZNS SSD
@@ -66,7 +78,7 @@ int main(int argc, char * argv[]) {
     cout<< "------------------------------------------------------" <<endl;
         if (atoi(argv[GC_NUMBER]) == 1) {
             cout << "Do Basic_zgc" << endl;
-            zns_simulation->basic_zgc();
+            //zns_simulation->basic_zgc();
         } else if (atoi(argv[GC_NUMBER]) == 2) {
             cout << "Do lsm_zgc" << endl;
             //zns_simulation->lsm_zgc();
